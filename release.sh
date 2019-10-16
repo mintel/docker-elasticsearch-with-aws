de3e53314dc32a2db51e88b11677a7fad041b764
Jaye Doepke <jdoepke@mintel.com>
2019-10-16 13:56:37 -0500

    Add release script

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+release() {
+    local version;
+    version="$1";
+
+    if ! git diff-index --exit-code --ignore-submodules HEAD > /dev/null; then
+        echo "ERROR: There are modified files in the git repository!";
+        exit 1;
+    fi
+
+    sed -i -E "s/^FROM .+/FROM docker.elastic.co\\/elasticsearch\\/elasticsearch:$version/" Dockerfile;
+    git add Dockerfile;
+    git commit -m "Elasticsearch v$version";
+    git tag -a -m "Elasticsearch v$version";
+}
+
+release "$@";
