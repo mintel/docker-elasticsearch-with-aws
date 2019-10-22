aabfb406dcee4b9ccd7b4363ce175070137a31a9
Jaye Doepke <jdoepke@mintel.com>
2019-10-22 10:22:43 -0500

    shfmt

--- release.sh
+++ release.sh
@@ -3,18 +3,18 @@
 set -euo pipefail
 
 release() {
-    local version;
-    version="$1";
+	local version
+	version="$1"
 
-    if ! git diff-index --exit-code --ignore-submodules HEAD > /dev/null; then
-        echo "ERROR: There are modified files in the git repository!";
-        exit 1;
-    fi
+	if ! git diff-index --exit-code --ignore-submodules HEAD >/dev/null; then
+		echo "ERROR: There are modified files in the git repository!"
+		exit 1
+	fi
 
-    sed -i -E "s/^FROM .+/FROM docker.elastic.co\\/elasticsearch\\/elasticsearch:$version/" Dockerfile;
-    git add Dockerfile;
-    git commit -m "Elasticsearch v$version";
-    git tag -a -m "Elasticsearch v$version";
+	sed -i -E "s/^FROM .+/FROM docker.elastic.co\\/elasticsearch\\/elasticsearch:$version/" Dockerfile
+	git add Dockerfile
+	git commit -m "Elasticsearch v$version"
+	git tag -a -m "Elasticsearch v$version"
 }
 
-release "$@";
+release "$@"
