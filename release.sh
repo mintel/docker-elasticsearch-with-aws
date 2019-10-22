c25deac5797bf36058b3e18f38ff794566c171fb
Jaye Doepke <jdoepke@mintel.com>
2019-10-22 10:23:12 -0500

    Error if trying to release a version that already exists

--- release.sh
+++ release.sh
@@ -6,6 +6,11 @@ release() {
 	local version
 	version="$1"
 
+	if git tag -l | grep "v$version" >/dev/null; then
+		echo "ERROR: version v$version already exists!"
+		exit 1
+	fi
+
 	if ! git diff-index --exit-code --ignore-submodules HEAD >/dev/null; then
 		echo "ERROR: There are modified files in the git repository!"
 		exit 1
